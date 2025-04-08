a99b3bf214cdfe7ea10f2808f9d0ce55e41ff198
BestenBergman <ola.bergman.1992@gmail.com>
2025-04-08 13:37:39 +0200

    Pussel1 fix

---
+++ Assets/Ola/Scripts/O_PusselOne.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public GameObject cube;
+    public GameObject pressurePlate;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == gameObject.tag)
+        {
+            Destroy(cube);
+        }
+    }
+}
