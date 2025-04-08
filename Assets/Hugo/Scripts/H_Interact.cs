f2dd152635be7e64a604cb3ae329e4c37c731118
Poison32 <123360205+Poison32@users.noreply.github.com>
2025-04-08 13:33:02 +0200

    PickUpSystem
    Gjorde plocka upp och släppa system i H_Interact och H_InputHandler

--- Assets/Hugo/Scripts/H_Interact.cs
+++ Assets/Hugo/Scripts/H_Interact.cs
@@ -1,11 +1,14 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
 
 public class H_Interact : MonoBehaviour
 {
+    [HideInInspector] public H_PlayerStats ps;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ps = GetComponent<H_PlayerStats>();
     }
 
     // Update is called once per frame
@@ -20,10 +23,44 @@ public class H_Interact : MonoBehaviour
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f,0.5f));
         if (Physics.Raycast(ray, out hit, 3f))
         {
-            if (hit.transform.tag == "Lever")
+            if (hit.transform.tag == "Item")
+            {
+                PickUpItem(hit.transform.gameObject);
+            }
+            else if (hit.transform.tag == "Lever")
             {
                 hit.transform.gameObject.GetComponent<SlutPussel>().SwitchState();
             }
+            else
+            {
+                DropItem();
+            }
         }
+        else
+        {
+            DropItem();
+        }
+    }
+
+    public void PickUpItem(GameObject item)
+    {
+        if (ps.hasItem)
+        {
+            gameObject.transform.GetChild(0).transform.GetChild(1).GetComponent<Rigidbody>().isKinematic = false;
+            gameObject.transform.GetChild(0).transform.GetChild(1).parent = gameObject.transform.parent;
+        }
+
+        ps.hasItem = true;
+        item.transform.parent = gameObject.transform.GetChild(0);
+        item.transform.localRotation = Quaternion.identity;
+        item.transform.localPosition = ps.HoldPos;
+        item.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    public void DropItem()
+    {
+        gameObject.transform.GetChild(0).transform.GetChild(1).GetComponent<Rigidbody>().isKinematic = false;
+        gameObject.transform.GetChild(0).transform.GetChild(1).parent = gameObject.transform.parent;
+        ps.hasItem = false;
     }
 }
---
+++ Assets/Hugo/Scripts/H_PlayerStats.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class H_PlayerStats : MonoBehaviour
+{
+    public bool hasItem = false;
+
+    public Vector3 HoldPos = new Vector3(0, 0, 2);
+}
