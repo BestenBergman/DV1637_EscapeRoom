32299080a1aecfb03e81b8b7ccfff2efbc524036
BestenBergman <ola.bergman.1992@gmail.com>
2025-04-09 11:17:41 +0200

    Gjort min keypad

--- Assets/Ola/Scripts/O_KeyPad.cs
+++ Assets/Ola/Scripts/O_KeyPad.cs
@@ -1,7 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class O_KeyPad : MonoBehaviour
-{
+
+    {
+    public TextMeshProUGUI input1;
+    public TextMeshProUGUI input2;
+    public TextMeshProUGUI input3;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,6 +27,18 @@ public class O_KeyPad : MonoBehaviour
     public void KeyPadPress()
     {
         Debug.Log(gameObject.name);
+        if (input1.text == "")
+        {
+            input1.text = gameObject.name;
+        }
+        else if (input2.text == "")
+        {
+            input2.text = gameObject.name;
+        }
+        else if (input3.text == "")
+        {
+            input3.text = gameObject.name;
+        }
     }
 
 }
