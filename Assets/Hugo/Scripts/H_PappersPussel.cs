169cf249ce570506a987679be4c9d48fe16a0689
Poison32 <123360205+Poison32@users.noreply.github.com>
2025-04-11 15:27:18 +0200

    LjusPussel2

--- Assets/Hugo/Scripts/H_PappersPussel.cs
+++ Assets/Hugo/Scripts/H_PappersPussel.cs
@@ -7,12 +7,13 @@ public class H_PappersPussel : MonoBehaviour
     public GameObject x_ax;
     public GameObject y_ax;
 
+    private MeshRenderer mr;
     public Material ingenKod;
     public Material visaKod;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        mr = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -23,13 +24,19 @@ public class H_PappersPussel : MonoBehaviour
         Vector3 Z = ljus.transform.position - transform.position;
         float det = Determinant(X, Y, Z);
 
-        if (det > 0)
+        if (-1f <= det && det < 0)
         {
-            Debug.Log("Se kod");
+            if (mr.material != visaKod)
+            {
+                mr.material = visaKod;
+            }
         }
         else
         {
-            Debug.Log("Inte");
+            if (mr.material != ingenKod)
+            {
+                mr.material = ingenKod;
+            }
         }
     }
 
