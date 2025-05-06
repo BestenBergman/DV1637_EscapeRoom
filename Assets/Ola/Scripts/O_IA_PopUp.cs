34397d093d27c6fdb0b03512e3ae72c587393702
BestenBergman <ola.bergman.1992@gmail.com>
2025-05-06 16:08:36 +0200

    PopUp script
    Gjort popup text för interract och implementerat.

--- Assets/Ola/Scripts/O_IA_PopUp.cs
+++ Assets/Ola/Scripts/O_IA_PopUp.cs
@@ -1,21 +1,85 @@
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class O_IA_PopUp : MonoBehaviour
 {
+    [HideInInspector] public H_PlayerStats ps;
+    public TextMeshProUGUI IA_PopUp;
+    public TextMeshProUGUI inspectPopUp;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ps = GameObject.FindGameObjectWithTag("Player").GetComponent<H_PlayerStats>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
-        if (Physics.Raycast(ray, out hit, 3.0f))
+        if (!ps.hasItem)
         {
+            inspectPopUp.text = "";
+            RaycastHit hit;
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
+            if (Physics.Raycast(ray, out hit, 3.0f))
+            {
+                if (ps.ItemTags.Contains(hit.transform.tag))
+                {
+                    IA_PopUp.text = "Press (E) to pick up item";
+                }
+                else if (hit.transform.tag == "R1_Keypad" || hit.transform.tag == "R2_Keypad")
+                {
+                    IA_PopUp.text = "Press (E) to activate keypad";
+                }
+                else if (hit.transform.tag == "R2_PuzzleLever" || hit.transform.tag == "Lever")
+                {
+                    IA_PopUp.text = "Press (E) to toggle lever";
+                }
+                else if (hit.transform.tag == "R3_Torch")
+                {
+                    IA_PopUp.text = "Press (E) to light torch";
+                }
+                else if (hit.transform.tag == "R4_Button")
+                {
+                    IA_PopUp.text = "Press (E) to press button";
+                }
+                else
+                {
+                    IA_PopUp.text = "";
+                }
 
+            }
+            else
+            {
+                IA_PopUp.text = "";
+            }
+        }
+        else if (ps.hasItem)
+        {
+            
+            
+            if(!ps.isInspecting)
+            {
+                inspectPopUp.text = "Press (Q) to inspect item";
+                IA_PopUp.text = "Press (E) to drop item";
+                if (ps.Shards.Contains(gameObject.transform.GetChild(0).transform.GetChild(1).tag))
+                {
+                    RaycastHit hit;
+                    Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
+                    if (Physics.Raycast(ray, out hit, 3.0f))
+                    {
+                        if (ps.ShardBoxes.Contains(hit.transform.tag))
+                        {
+                            IA_PopUp.text = "Press (E) to place shard";
+                        }
+                    }    
+                }
+            }
+            else if (ps.isInspecting)
+            {
+                IA_PopUp.text = "";
+                inspectPopUp.text = "Press (Q) to uninspect item";
+            }
         }
     }
 }
