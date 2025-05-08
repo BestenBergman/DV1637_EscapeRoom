f18384c1b8e42ae49a647d6383dd3e050727fdaf
BestenBergman <ola.bergman.1992@gmail.com>
2025-05-08 15:36:52 +0200

    Sand particle

--- Assets/Ola/Scripts/O_FallingSand.cs
+++ Assets/Ola/Scripts/O_FallingSand.cs
@@ -1,22 +1,34 @@
+using System.Threading;
 using UnityEngine;
 
 public class O_FallingSand : MonoBehaviour
 {
     public Material sand;
+    private float timer;
+    public ParticleSystem fsps;
+    float amountOfSand = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
+        //var emission = fsps.emission;
         Color color = sand.color;
-        color.a = 0.25f;
+        color.a = 0.2f;
         sand.color = color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Color color = sand.color;
-        color.a += 0.1f * Time.deltaTime;
-        sand.color = color;
+        timer += Time.deltaTime;
+        var emission = fsps.emission;
+        if (timer > 20)
+        {
+            emission.rateOverTime = amountOfSand;
+            Color color = sand.color;
+            color.a += 0.1f * Time.deltaTime;
+            sand.color = color;
+        }
     }
 }
