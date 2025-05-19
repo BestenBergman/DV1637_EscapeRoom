088d95a554c125912f84ba34213993a407c6d924
Theo <theowien02@gmail.com>
2025-05-19 09:37:19 +0200

    code fix

--- Assets/Theo/Scripts/T_TriggerEnd.cs
+++ Assets/Theo/Scripts/T_TriggerEnd.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 
+
 public class T_TriggerEnd : MonoBehaviour
 {
     public bool Win = false;
     public GameObject UI;
 
     private T_HourglassTimer hourglassTimer;
+
     private void Start()
     {
         hourglassTimer = UI.GetComponent<T_HourglassTimer>();
