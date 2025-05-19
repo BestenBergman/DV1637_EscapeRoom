f53f4f9c029e25b4df827cc5c42210e7ad9007b8
Poison32 <123360205+Poison32@users.noreply.github.com>
2025-05-19 09:36:45 +0200

    Dörr ljud

--- Assets/Theo/Scripts/T_AnimControl.cs
+++ Assets/Theo/Scripts/T_AnimControl.cs
@@ -5,6 +5,9 @@ public class T_AnimControl : T_ToggleControl
 {
     protected Animator anim;
 
+    [SerializeField] private AudioClip audioClip;
+    private bool playedSound = false;
+
     protected override void Start()
     {
         anim = GetComponent<Animator>();
@@ -19,6 +22,11 @@ public class T_AnimControl : T_ToggleControl
 
     protected override void SetObjectState()
     {
+        if (audioClip != null && isToggled && !playedSound)
+        {
+            playedSound = true;
+            H_SoundFXManager.instance.PlaySoundFXClip(audioClip, transform.GetChild(0), 1f);
+        }
         if (anim != null)
         {
             anim.SetBool("isToggled", isToggled);
