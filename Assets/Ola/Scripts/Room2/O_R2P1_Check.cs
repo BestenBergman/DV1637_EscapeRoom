c9eb1124a00c1e1056de29228a45bd33e46e45dc
Poison32 <123360205+Poison32@users.noreply.github.com>
2025-05-19 11:15:02 +0200

    Spak ljud och bakgrunds ljud

--- Assets/Ola/Scripts/Room2/O_R2P1_Check.cs
+++ Assets/Ola/Scripts/Room2/O_R2P1_Check.cs
@@ -16,6 +16,8 @@ public class O_R2P1_Check : MonoBehaviour
     [Tooltip("The door that opens when the puzzle is complete")]
     public GameObject gateDoor;
 
+    [SerializeField] private AudioClip doorSound;
+
 
     void Update()
     {
@@ -39,6 +41,7 @@ public class O_R2P1_Check : MonoBehaviour
         if (lever1Check && !lever2Check && !lever3Check && lever4Check)
         {
             r2p1Completed = true;
+            H_SoundFXManager.instance.PlaySoundFXClip(doorSound, gateDoor.transform, 1f);
             gateDoor.SetActive(false);
         }
     }
